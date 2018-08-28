92d16b0a441f692396df59529b1056c9549be8d7
Carlos Henrique <carlosbda99@hotmail.com>
2018-08-28 20:03:11 -0300

    SSMS v2.0f

--- suap.py
+++ suap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 
 # Importing libraries
 import requests
@@ -33,6 +33,7 @@ def main(token):
     print("Bem vindo ao Sistema de Suporte de Material do SUAP")
     year = input("Digite o ano que deseja realizar o download de materiais: ")
     period = input("Selecione o período desejado: ")
+    print('Realizando download, aguarde...')
     if not (period == '1' or period == '2'):
         print("Entrada incorreta, tente novamente (1 ou 2)")
         sleep(2)
@@ -47,25 +48,28 @@ def main(token):
         sleep(2)
         system('clear')
         main()
-    for x in grade:
-        if x!='detail':
-            codAd=str(x['codigo_diario'])
-            queue = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/turma-virtual/%s/'%codAd, headers={'Authorization' : 'JWT ' + token})
-            if queue.status_code == 200:
-                classV = queue.json()
-                directory = x['disciplina'].replace('/','-').split(' ')
-                directory = '_'.join(directory)
-                system ('mkdir -p \"%s-%s/%s\"'%(year,period,directory))
-                for x in classV['materiais_de_aula']:
-                    if x['url'][0]=='/':
-                        link = 'https://suap.ifrn.edu.br' +(x['url'])
-                        system ('wget -N -P \"%s-%s/%s\" \"%s\"'%(year,period,directory,link))
-                    else:
-                        link = x['url']
-                        system ('echo \"%s\" > \"%s-%s/%s/Link_de_naterial\"'%(link,year,period,directory))
-                print('Download realizado')
-            elif queue.status_code == 404:
-                print('Erro, tente novamente')
+    try:
+        for x in grade:
+            if x!='detail':
+                codAd=str(x['codigo_diario'])
+                queue = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/turma-virtual/%s/'%codAd, headers={'Authorization' : 'JWT ' + token})
+                if queue.status_code == 200:
+                    classV = queue.json()
+                    directory = x['disciplina'].replace('/','-').split(' ')
+                    directory = '_'.join(directory)
+                    system ('mkdir -p \"%s-%s/%s\"'%(year,period,directory))
+                    for x in classV['materiais_de_aula']:
+                        if x['url'][0]=='/':
+                            link = 'https://suap.ifrn.edu.br' +(x['url'])
+                            system ('wget -nv -N -P \"%s-%s/%s\" \"%s\"'%(year,period,directory,link))
+                        else:
+                            link = x['url']
+                            system ('echo \"%s\" > \"%s-%s/%s/Link_de_naterial\"'%(link,year,period,directory))
+                elif queue.status_code == 404:
+                    print('Erro, tente novamente')
+        print('Download realizado')
+    except:
+        print('Houve algum erro ao realizar o download, desculpe!')
 
 #main()
 authentication()
